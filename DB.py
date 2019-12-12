1e97f54e448bd0677253cc4b7bb0cca6f1c490c5
keita-t <key.tashima@gmail.com>
2019-12-12 13:28:20 +0900

    12/12/2019

--- DB.py
+++ DB.py
@@ -240,6 +240,7 @@ sqlalchemy_orm()
 # NoSQL データストア
 #
 
+
 # dbm ファミリ (キーバリューストア)
 def dbm_datastore():
     import dbm
@@ -278,6 +279,7 @@ def memcached():
 # Redis (データ構造サーバー)
 def redis():
     import redis
+    import time
     conn = redis.Redis('localhost', 6379)  # redis-server に接続
 
     # 文字列
@@ -309,7 +311,104 @@ def redis():
     print(conn.incrbyfloat('fever', 0.5))  # 102.5+0.5=103.0
     print(conn.incrbyfloat('fever', -2.0))  # 実質デクリメント(103.0-2.0=101.0)
 
+    conn.flushall()
+
     # リスト
+    print(conn.lpush('zoo', 'bear'))  # リストzoozの先頭にbearを追加(1)
+    print(conn.lpush('zoo', 'alligator', 'duck'))  # 先頭に複数の要素を追加(3)
+    print(conn.lrange('zoo', 0, 2))  # duck, alligator, bear の順になっている
+    print(conn.linsert(
+        'zoo', 'before', 'bear', 'beaver'))  # bearの前にbeaverを追加(4)
+    print(conn.linsert(
+        'zoo', 'after', 'bear', 'cassowary'))  # bearの後にcassowaryを追加(5)
+    print(conn.lset('zoo', 2, 'marmoset'))  # index 2 の位置にmarmosetを挿入(True)
+    print(conn.rpush('zoo', 'yak'))  # 末尾にyakを追加(6)
+    print(conn.lindex('zoo', 3))  # 指定したオフセットの値を得る(b'bear')
+    print(conn.lrange('zoo', 0, 2))  # 範囲(b'duck',b'alligator',b'marmoset')
+    print(conn.ltrim('zoo', 1, 4))  # リストの刈り込み,指定されたオフセットの範囲の要素が残る(True)
+    print(conn.lrange('zoo', 0, -1))  # 0から-1で全体の値を得る
+
+    conn.flushall()
+
+    # ハッシュ
+    print(conn.hmset('song', {
+        'do': 'a deer', 're': 'about a deer'}))  # songハッシュにdo,reを追加(True)
+    print(conn.hset('song', 'mi', 'a note to follow re'))  # miキーの値を追加(1)
+    print(conn.hget('song', 'mi'))  # miキーの値を取得(b'a note to follow re')
+    print(conn.hmget(
+        'song', 're', 'do'))  # 複数のキー(re,do)から取得([b'about a beer', b'a deer'])
+    print(conn.hkeys('song'))  # すべてのキーを取得([b'do', b're', b'mi'])
+    print(conn.hvals('song'))  # すべての値([b'a deer', ... b'a note to follow re'])
+    print(conn.hvals('song'))  # フィールドの数を取得(3)
+    print(conn.hgetall('song'))  # すべてのキーと値を取得({b'do': b'a deer', ... })
+    print(conn.hsetnx('song', 'fa', 'a note that rhymes with la'))  # キーがなければ設定
+
+    conn.flushall()
+
+    # 集合
+    print(conn.sadd(
+        'zoo', 'duck', 'goat', 'turkey'))  # duck,goat,turkeyの集合zoo(3)
+    print(conn.scard('zoo'))  # 集合の値の数を取得(3)
+    print(conn.smembers('zoo'))  # 集合のすべての値を取得({b'duck', b'goat', b'turkey'})
+    print(conn.srem('zoo', 'turkey'))  # turkeyを取り除く(1)
+    print(conn.sadd('better_zoo', 'tiger', 'wolf', 'duck'))  # 集合better_zooを作る
+    print(conn.sinter('zoo', 'better_zoo'))  # zooとbetter_zooの積集合({b'duck'})
+    print(conn.sinterstore(
+        'fowl_zoo', 'zoo', 'better_zoo'))  # fowl_zooにzooとbetter_zooの積集合を格納(1)
+    print(conn.smembers('fowl_zoo'))  # {b'duck'}
+    print(conn.sunion(
+        'zoo', 'better_zoo'))  # zooとbetter_zooの和集合({b'duck', ... b'tiger'})
+    print(conn.sunionstore(
+        'fabulous_zoo', 'zoo', 'better_zoo'))  # fablous_zooに和集合を格納(4)
+    print(conn.smembers('fabulous_zoo'))  # {b'duck',b'goat',b'wolf',b'tiger'}
+    print(conn.sdiff('zoo', 'better_zoo'))  # zooとbetter_zooの差集合({b'goat'})
+    print(conn.sdiffstore(
+        'zoo_sale', 'zoo', 'better_zoo'))  # zoo_saleにzooとbetter_zooの差集合を格納(1)
+    print(conn.smembers('zoo_sale'))  # {b'goat'}
+
+    conn.flushall()
+
+    # ソート済み集合
+    now = time.time()  # 現在時間を基にしてソート
+    print(conn.zadd('logins', {'smeagol': now}))  # logins集合に最初のゲストsmeagolを追加
+    print(conn.zadd('logins', {'sauron': now+(5*60)}))  # 5分後に次のゲスト(sauron)
+    print(conn.zadd('logins', {'bilbo': now+(2*60*60)}))  # 2時間後に次のゲスト(bilbo)
+    print(conn.zadd('logins', {'treebeard': now+(24*60*60)}))  # 1日後(treebeard)
+    print(conn.zrank('logins', 'bilbo'))  # bilboがやってきたのは2番目(2)
+    print(conn.zscore('logins', 'bilbo'))  # それは何時か(now+(24*60*60))
+    print(conn.zrange('logins', 0, -1))  # 全員をログイン順に見る([b'smeagol', ... ])
+    print(conn.zrange('logins', 0, -1, withscores=True))  # 時間付きでログイン順
+
+    conn.flushall()
+
+    # ビット
+    days = ['2013-02-25', '2013-02-26', '2013-02-27']  # 3日間
+    big_spender = 1089      # ID
+    tire_kicker = 40459     # ID
+    late_joiner = 550212    # ID
+
+    print(conn.setbit(days[0], big_spender, 1))  # 最初の日にbig_spenderがログイン(0)
+    print(conn.setbit(days[0], tire_kicker, 1))  # 最初の日にtire_kickerがログイン(0)
+    print(conn.setbit(days[1], big_spender, 1))  # 次の日もbig_spenderがログイン(0)
+    print(conn.setbit(days[2], big_spender, 1))  # 3日目もbig_spenderがログイン(0)
+    print(conn.setbit(days[2], late_joiner, 1))  # 3日目にlate_joinerがログイン(0)
+    for day in days:
+        conn.bitcount(day)  # この３日の各日の訪問者数(2, 1, 2)
+    print(conn.getbit(days[1], tire_kicker))  # tire_kickerは２日目には来ていない(0)
+    conn.bitop('and', 'everyday', *days)  # 毎日ログインしてきたユーザー
+    print(conn.bitcount('everyday'))  # １人(1)
+    print(conn.getbit('everyday', big_spender))  # big_spenderだった(1)
+    conn.bitop('or', 'alldays', *days)  # ３日間のユニークユーザーの合計
+    print(conn.bitcount('alldays'))  # 3人(3)
+
+    # キーの有効期限
+    key = 'now you see it'
+    conn.set(key, 'but not for long')
+    print(conn.expire(key, 5))  # キーを５秒間有効にする(True)
+    print(conn.ttl(key))  # 5
+    print(conn.get(key))  # まだ生存している(b'but not for long')
+    time.sleep(6)  # 6秒間スリープ
+    print(conn.get(key))  # 期限切れでもうないので取得できない
 
 
-# redis()
+redis()
